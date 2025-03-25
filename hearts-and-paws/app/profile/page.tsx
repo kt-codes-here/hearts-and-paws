e863285d36f9457d91b25c2a1099b166602e60cf
Ritik Kale <65969779+ItsRitik@users.noreply.github.com>
2025-03-25 01:59:44 -0400

    fixes

--- hearts-and-paws/app/profile/page.tsx
+++ hearts-and-paws/app/profile/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import UserProfile from '@/components/global/userProfile'
+import UserProfile from '@/components/global/UserProfile'
 import { useUser } from '@clerk/nextjs'
 import React from 'react'
 
